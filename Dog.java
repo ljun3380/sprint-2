b036291cc2354bb836e2b0fb643dbe3a707517c0
ljun3380 <ljun3380@gmail.com>
2025-10-15 18:24:20 -0400

    Add files via upload

--- Dog.java
+++ Dog.java
@@ -12,11 +12,13 @@ public class Dog {
         this.breed = breed_;
     }
 
+    // Getters and Setters
     public String getName() { return this.name; }
     public void setName(String name_) { this.name = name_; }
     public int getAge() { return this.age; }
     public void setAge(int age_) { this.age = (age_ > 0) ? age_ : 1; }
 
+    // Custom methods
     public String dogReport() {
         String[] report_text = new String[2];
         report_text[0] = getName();
@@ -24,7 +26,7 @@ public class Dog {
         return "Dog's name is " + report_text[0] + " and is " + report_text[1] + " years old.";
     }
 
-    // main class for testing
+    // Main class for testing
     public static void main(String[] args) {
         Dog my_pup = new Dog("Barney", 13, 4, "Shi Tzu");
         System.out.println(my_pup.dogReport());
